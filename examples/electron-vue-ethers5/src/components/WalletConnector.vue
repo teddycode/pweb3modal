cf3d3fc3e7e325444bb0ad3f74c80a21dcbc5cd7
teddycode <1055334354@qq.com>
2024-07-27 23:37:35 +0800

    feat: electron platform example supported

---
+++ examples/electron-vue-ethers5/src/components/WalletConnector.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { useWeb3Modal,createWeb3Modal, defaultConfig } from '@web3modal/ethers5/vue'
+
+// 1. Get projectId at https://cloud.walletconnect.com
+const projectId = '343fd684b4e15b05c6c532d431a4a13a'
+
+// 2. Set chains
+const mainnet = {
+  chainId: 1,
+  name: 'Ethereum',
+  currency: 'ETH',
+  explorerUrl: 'https://etherscan.io',
+  rpcUrl: 'https://cloudflare-eth.com'
+}
+
+// 3. Create modal
+const metadata = {
+  name: 'My Website',
+  description: 'My Website description',
+  url: 'https://mywebsite.com',
+  icons: ['https://avatars.mywebsite.com/']
+}
+
+createWeb3Modal({
+  ethersConfig: defaultConfig({ metadata }),
+  chains: [mainnet],
+  projectId
+})
+// 4. Use modal composable
+const modal = useWeb3Modal()
+
+</script>
+
+<template>
+  <button @click="modal.open()">Open Connect Modal</button>
+  <button @click="modal.open({ view: 'Networks' })">Open Network Modal</button>
+</template>
